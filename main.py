25baddcf2cf30bca1bdefbe4341e37d45b70fe86
blueskyson <blueskyson1401@gmail.com>
2022-01-16 01:19:26 +0800

    Finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import cv2
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
@@ -11,14 +12,13 @@ import torch
 import torchvision
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import image
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
-        # self.setGeometry(200, 200, 640, 400)
         self.setWindowTitle("cifar10-vgg16")
-        
+
         button = [None] * 5
         button[0] = QPushButton("1 show random labeled images", self)
         button[0].clicked.connect(self.show_random_labeled_images)
@@ -28,36 +28,39 @@ class MainWindow(QWidget):
         button[2].clicked.connect(self.show_training_accuracy)
         button[3] = QPushButton("4 show test", self)
         button[3].clicked.connect(self.show_test)
-        self.index_text = QLineEdit("0")
+        self.index_txt = QLineEdit("0")
 
         vbox = QVBoxLayout()
         vbox.addWidget(button[0])
         vbox.addWidget(button[1])
         vbox.addWidget(button[2])
-        vbox.addWidget(self.index_text)
+        vbox.addWidget(self.index_txt)
         vbox.addWidget(button[3])
         vbox.addStretch(1)
         self.setLayout(vbox)
-
+        
+        print("Loading data...")
+        self.trainset, self.testset, self.classes = vgg16train.load_data()
+    
     # Show images. Take reference from
     # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html?highlight=cifar
     def show_random_labeled_images(self):
-        trainset, testset, classes = vgg16train.load_data()
-        trainloader = torch.utils.data.DataLoader( \
-            trainset, batch_size=9, shuffle=True, num_workers=2)
-        
+        trainloader = torch.utils.data.DataLoader(
+            self.trainset, batch_size=9, shuffle=True, num_workers=2
+        )
+
         # get some random training images
         dataiter = iter(trainloader)
         images, labels = dataiter.next()
         images = torchvision.utils.make_grid(images, nrow=3)
-        images = images / 2 + 0.5     # unnormalize
+        images = images / 2 + 0.5  # unnormalize
         npimg = images.numpy()
 
         # generate label
         s = ""
         i = 1
         for l in labels:
-            s += str(i) + "." + str(classes[l]) + " "
+            s += str(i) + "." + str(self.classes[l]) + " "
             i += 1
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.title(s)
@@ -67,19 +70,15 @@ class MainWindow(QWidget):
         print(vgg16train.get_model())
 
     def show_training_accuracy(self):
-        # vgg16train.train(
-        #     self.batch_size,
-        #     self.learning_rate,
-        #     20
-        # )
-        accimg = image.imread("accuracy.png")
-        lossimg = image.imread("loss.png")
-        plt.imshow(accimg)
-        plt.imshow(lossimg)
-    
+        accimg = cv2.imread("accuracy.png")
+        lossimg = cv2.imread("loss.png")
+        cv2.imshow("Accuracy", accimg)
+        cv2.imshow("Loss", lossimg)
+
     def show_test(self):
         id = int(self.index_txt.text())
-        vgg16train.test(id)
+        vgg16train.test(id, self.testset, self.classes)
+
 
 def main():
     app = QApplication(sys.argv)
--- vgg16train.py
+++ vgg16train.py
@@ -1,29 +1,46 @@
 import os
 import numpy as np
 import torch
-import torchvision
 from torchvision import models
 from torchvision import transforms
 from torchvision import datasets
 import torch.nn as nn
-import torch.backends.cudnn as cudnn
+from tqdm import tqdm
 from matplotlib import pyplot as plt
 
+BATCH_SIZE = 128
+LEARNING_RATE = 0.01
+EPOCH = 20
+FILENAME = "vgg16.pth"
+WORKING_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # load CIFAR10. Take reference from
 # https://clay-atlas.com/blog/2019/10/20/pytorch-chinese-tutorial-classifier-cifar-10/
 def load_data():
-    transform = transforms.Compose([transforms.ToTensor(), \
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    
-    trainset = datasets.CIFAR10( \
-    root='.', train=True, download=True, transform=transform)
-    
-    testset = datasets.CIFAR10( \
-    root='.', train=False, download=True, transform=transform)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', \
-    'dog', 'frog', 'horse', 'ship', 'truck')
+    trainset = datasets.CIFAR10(
+        root=".", train=True, download=True, transform=transform
+    )
+
+    testset = datasets.CIFAR10(
+        root=".", train=False, download=True, transform=transform
+    )
+
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
 
     return trainset, testset, classes
 
@@ -32,99 +49,148 @@ def load_data():
 def get_model():
     model = models.vgg16()
     # only use 10 features
-    model.classifier._modules['6'] = nn.Linear(in_features=4096, out_features=10, bias=True)
+    model.classifier._modules["6"] = nn.Linear(
+        in_features=4096, out_features=10, bias=True
+    )
     return model
 
-
-# train model
-def train(batch_size, learning_rate, epoch):
-    trainset, testset, classes = load_data()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = get_model().to(device)
-    trainloader = torch.utils.data.DataLoader(
-        trainset,
-        batch_size = 32,
-        shuffle = True,
-        num_workers = 4
-    )
+def train(trainloader, model, criterion, optimizer):
+    model.train()
+    losses = 0.0
+    corrects = 0
+    total = 0
     
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-    criterion = nn.CrossEntropyLoss()
-    acc_list = []
-    loss_list = []
-
-    for i in range(epoch):
-        print(f"\nEpoch: {i}")
-        train_loss = 0
-        correct = 0
-        total = 0
+    for inputs, targets in tqdm(trainloader):
+        inputs = inputs.cuda(non_blocking=True)
+        targets = targets.cuda(non_blocking=True)
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
         
-        # train using trainset 
-        for batch_idx, (inputs, targets) in enumerate(trainloader):
-            # Here inputs are images and targets are labels
-            inputs, targets = inputs.to(device), targets.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            
-            train_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-            tmp = tmp + 1
-        acc = correct * 100.0 / total
-        loss = train_loss / batch_idx
-        print(f"Loss: {loss:.2f} | Acc: {acc:.2f}%")
-        acc_list.append(acc)
-        loss_list.append(loss)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    torch.save(model.state_dict(), os.path.join(pwd,'vgg16.pth'))
-    print("Save model to ./vgg16.pth")
+        losses += loss.item()
+        _, predicted = outputs.max(1)
+        corrects += predicted.eq(targets).sum().item()
+        total += targets.size(0)
+    
+    train_acc = corrects * 100.0 / total  # percentage
+    train_loss = losses / len(trainloader)
+    return train_acc, train_loss
 
-    # Save accuracy as an image
-    x = list(range(epoch))
-    plt.clf()
-    plt.plot(x, acc_list)
-    plt.title('Accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.savefig('accuracy.png')
+def validate(validateloader, model, criterion):
+    model.eval()
+    losses = 0.0
+    corrects = 0
+    total = 0
 
-    # Save loss as an image
-    plt.clf()
-    plt.plot(x, loss_list)
-    plt.title('Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.savefig('loss.png')
+    for inputs, targets in tqdm(validateloader):
+        inputs = inputs.cuda(non_blocking=True)
+        targets = targets.cuda(non_blocking=True)
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        
+        losses += loss.item()
+        _, predicted = outputs.max(1)
+        corrects += predicted.eq(targets).sum().item()
+        total += targets.size(0)
+    
+    validate_acc = corrects * 100.0 / total  # percentage
+    validate_loss = losses / len(validateloader)
+    return validate_acc, validate_loss
 
-
-def test(index):
-    trainset, testset, classes = load_data()
+def test(index, dataset, classes):
     model = get_model()
-    state_dict = torch.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),f'vgg16.pth'))
+    state_dict = torch.load(
+        os.path.join(WORKING_DIR, FILENAME)
+    )
     model.load_state_dict(state_dict)
     model.eval()
 
     # Load selected image
-    img = testset[index][0]
+    img = dataset[index][0]
     norm_img = img / 2 + 0.5
     norm_img = norm_img.numpy()
     plt.figure()
     plt.imshow(np.transpose(norm_img, (1, 2, 0)))
     plt.show()
-    
+
     # Generate predict chart
     output = model(img.unsqueeze(0))
     softmax = torch.nn.Softmax()
-    probabilities = softmax(output) 
-    
+    probabilities = softmax(output)
+
     plt.figure()
     plt.bar(np.asarray(classes), probabilities.detach().numpy()[0])
     plt.show()
 
+
+def main():
+    trainset, testset, classes = load_data()
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4
+    )
+
+    model = get_model().cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)\
+    
+    best_acc = 0.0
+    train_accs = []
+    validate_accs = []
+    train_losses = []
+    validate_losses = []
+    
+    for epoch in range(EPOCH):
+        print(f"\nEPOCH {epoch + 1}:")
+        print("Train:")
+        tr_acc, tr_loss = train(trainloader, model, criterion, optimizer)
+        print("Validate:")
+        va_acc, va_loss = validate(testloader, model, criterion)
+        print(f"train accuarcy: {tr_acc}, train loss: {tr_loss} | "
+              f"validate accuracy: {va_acc}, validate loss: {va_loss}")
+        
+        train_accs.append(tr_acc)
+        train_losses.append(tr_loss)
+        validate_accs.append(va_acc)
+        validate_losses.append(va_loss)
+
+        if (va_acc > best_acc):
+            torch.save(
+                model.state_dict(),
+                os.path.join(WORKING_DIR, FILENAME)
+            )
+    
+    # Save accuracy as an image
+    plt.clf()
+    plt.plot(train_accs, "g", label="Training Acc")
+    plt.plot(validate_accs, "b", label="Validation Acc")
+    plt.title("Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.grid()
+    plt.savefig("accuracy.png")
+
+    # Save loss as an image
+    plt.clf()
+    plt.plot(train_losses, "g", label="Training Loss")
+    plt.plot(validate_losses, "b", label="Validation Loss")
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.grid()
+    plt.savefig("loss.png")
+
+
 if __name__ == "__main__":
-    train(32, 0.01, 20)
+    if not torch.cuda.is_available():
+        print("Cuda not available, abort.")
+    else:
+        main()
+
